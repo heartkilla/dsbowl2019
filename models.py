c3f2b22d8aab7d2f395dbe9e468613cd6ca62b23
heartkilla <artem.official.by@gmail.com>
2019-12-08 19:39:50 +0900

    CV random QWK: 0.547822+/-0.030076

--- models.py
+++ models.py
@@ -47,19 +47,8 @@ class LGBMModel:
         for n_fold, (tr_index, val_index) in enumerate(self.folds.split(X, y, X[self.group_col])):
             print(f'Fold {n_fold + 1}:')
 
-            X_tr, X_val = X.iloc[tr_index], X.iloc[val_index]
-            y_tr, y_val = y.iloc[tr_index], y.iloc[val_index]
-
-            for col in self.map_groups:
-                mapping = X_tr.groupby('title')[col].mean()
-                self.title_mappings.append(mapping)
-                X_tr[col + '_mean_title'] = X_tr[col].map(mapping)
-                X_val[col + '_mean_title'] = X_val[col].map(mapping)
-
-                mapping = X_tr.groupby('world')[col].mean()
-                self.world_mappings.append(mapping)
-                X_tr[col + '_mean_world'] = X_tr[col].map(mapping)
-                X_val[col + '_mean_world'] = X_val[col].map(mapping)
+            X_tr, X_val = X.iloc[tr_index].groupby('installation_id').apply(lambda x: x.sample(1, random_state=707)).reset_index(drop=True), X.iloc[val_index]
+            y_tr, y_val = X_tr['accuracy_group'], y.iloc[val_index]
 
             X_rands = [X_val.groupby('installation_id').apply(lambda x: x.sample(1, random_state=i)).reset_index(drop=True) for i in range(5)]
             y_rands = [X_rand['accuracy_group'] for X_rand in X_rands]
@@ -85,15 +74,99 @@ class LGBMModel:
 
             self.models.append(model)
 
-            val_pred = model.predict(X_val)
-            val_pred = tr_mean + (val_pred - val_pred.mean()) / (val_pred.std() / tr_std)
-            thresholds = [0.5, 1.5, 2.5]
-            val_pred = allocate_to_rate(val_pred, thresholds)
-            self.oof_train[val_index] = val_pred
+            X_tr = X.iloc[tr_index].groupby('installation_id').apply(lambda x: x.sample(1, random_state=708)).reset_index(drop=True)
+            y_tr = X_tr['accuracy_group']
+            X_tr = X_tr.drop(columns=self.cols_to_drop)
+            d_tr = lgb.Dataset(X_tr, y_tr)
+            d_val = lgb.Dataset(X_val, y_val)
+            tr_mean_1 = y_tr.mean()
+            self.tr_means.append(tr_mean_1)
+            tr_std_1 = y_tr.std()
+            self.tr_stds.append(tr_std_1)
+            model_1 = lgb.train(self.params, d_tr,
+                              valid_sets=[d_tr, d_val],
+                              feval=partial(eval_qwk_lgb_regr, tr_mean=tr_mean_1, tr_std=tr_std_1),
+                              num_boost_round=self.num_boost_round,
+                              early_stopping_rounds=self.early_stopping_rounds,
+                              verbose_eval=self.verbose_eval,
+                              categorical_feature=['world'])
+            self.models.append(model_1)
+
+            X_tr = X.iloc[tr_index].groupby('installation_id').apply(lambda x: x.sample(1, random_state=709)).reset_index(drop=True)
+            y_tr = X_tr['accuracy_group']
+            X_tr = X_tr.drop(columns=self.cols_to_drop)
+            d_tr = lgb.Dataset(X_tr, y_tr)
+            d_val = lgb.Dataset(X_val, y_val)
+            tr_mean_2 = y_tr.mean()
+            self.tr_means.append(tr_mean_2)
+            tr_std_2 = y_tr.std()
+            self.tr_stds.append(tr_std_2)
+            model_2 = lgb.train(self.params, d_tr,
+                              valid_sets=[d_tr, d_val],
+                              feval=partial(eval_qwk_lgb_regr, tr_mean=tr_mean_2, tr_std=tr_std_2),
+                              num_boost_round=self.num_boost_round,
+                              early_stopping_rounds=self.early_stopping_rounds,
+                              verbose_eval=self.verbose_eval,
+                              categorical_feature=['world'])
+            self.models.append(model_2)
+
+            X_tr = X.iloc[tr_index].groupby('installation_id').apply(lambda x: x.sample(1, random_state=710)).reset_index(drop=True)
+            y_tr = X_tr['accuracy_group']
+            X_tr = X_tr.drop(columns=self.cols_to_drop)
+            d_tr = lgb.Dataset(X_tr, y_tr)
+            d_val = lgb.Dataset(X_val, y_val)
+            tr_mean_3 = y_tr.mean()
+            self.tr_means.append(tr_mean_3)
+            tr_std_3 = y_tr.std()
+            self.tr_stds.append(tr_std_3)
+            model_3 = lgb.train(self.params, d_tr,
+                              valid_sets=[d_tr, d_val],
+                              feval=partial(eval_qwk_lgb_regr, tr_mean=tr_mean_3, tr_std=tr_std_3),
+                              num_boost_round=self.num_boost_round,
+                              early_stopping_rounds=self.early_stopping_rounds,
+                              verbose_eval=self.verbose_eval,
+                              categorical_feature=['world'])
+            self.models.append(model_3)
+
+            X_tr = X.iloc[tr_index].groupby('installation_id').apply(lambda x: x.sample(1, random_state=711)).reset_index(drop=True)
+            y_tr = X_tr['accuracy_group']
+            X_tr = X_tr.drop(columns=self.cols_to_drop)
+            d_tr = lgb.Dataset(X_tr, y_tr)
+            d_val = lgb.Dataset(X_val, y_val)
+            tr_mean_4 = y_tr.mean()
+            self.tr_means.append(tr_mean_4)
+            tr_std_4 = y_tr.std()
+            self.tr_stds.append(tr_std_4)
+            model_4 = lgb.train(self.params, d_tr,
+                              valid_sets=[d_tr, d_val],
+                              feval=partial(eval_qwk_lgb_regr, tr_mean=tr_mean_4, tr_std=tr_std_4),
+                              num_boost_round=self.num_boost_round,
+                              early_stopping_rounds=self.early_stopping_rounds,
+                              verbose_eval=self.verbose_eval,
+                              categorical_feature=['world'])
+            self.models.append(model_4)
+
+
+            #val_pred = model.predict(X_val)
+            #val_pred = tr_mean + (val_pred - val_pred.mean()) / (val_pred.std() / tr_std)
+            #thresholds = [0.5, 1.5, 2.5]
+            #val_pred = allocate_to_rate(val_pred, thresholds)
+            #self.oof_train[val_index] = val_pred
 
             for i in range(5):
                 val_pred = model.predict(X_rands[i])
                 val_pred = tr_mean + (val_pred - val_pred.mean()) / (val_pred.std() / tr_std)
+
+                val_pred_1 = model_1.predict(X_rands[i])
+                val_pred_1 = tr_mean_1 + (val_pred_1 - val_pred_1.mean()) / (val_pred_1.std() / tr_std_1)
+                val_pred_2 = model_2.predict(X_rands[i])
+                val_pred_2 = tr_mean_2 + (val_pred_2 - val_pred_2.mean()) / (val_pred_2.std() / tr_std_2)
+                val_pred_3 = model_3.predict(X_rands[i])
+                val_pred_3 = tr_mean_3 + (val_pred_3 - val_pred_3.mean()) / (val_pred_3.std() / tr_std_3)
+                val_pred_4 = model_4.predict(X_rands[i])
+                val_pred_4 = tr_mean_4 + (val_pred_4 - val_pred_4.mean()) / (val_pred_4.std() / tr_std_4)
+                val_pred = (val_pred + val_pred_1 + val_pred_2 + val_pred_3 + val_pred_4) / 5
+
                 thresholds = [0.5, 1.5, 2.5]
                 val_pred = allocate_to_rate(val_pred, thresholds)
                 oof_rand.extend(val_pred)
@@ -119,9 +192,9 @@ class LGBMModel:
         preds = np.zeros(X.shape[0])
 
         for i, model in enumerate(self.models):
-            for col in self.map_groups:
-                X[col + '_mean_title'] = X[col].map(self.title_mappings[i])
-                X[col + '_mean_world'] = X[col].map(self.world_mappings[i])
+            #for col in self.map_groups:
+            #    X[col + '_mean_title'] = X[col].map(self.title_mappings[i])
+            #    X[col + '_mean_world'] = X[col].map(self.world_mappings[i])
             pred = model.predict(X)
             pred = self.tr_means[i] + (pred - pred.mean()) / (pred.std() / self.tr_stds[i])
             preds += pred
